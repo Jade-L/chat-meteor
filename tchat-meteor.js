aaad4c9cc32d40314a1ac2ce8e097f700a474111
Jade-L <lemaissi.jade@gmail.com>
2016-03-01 11:29:36 +0100

    add def messages in .js & textarea in .html

--- tchat-meteor.js
+++ tchat-meteor.js
@@ -1,20 +1,11 @@
+Messages = new Mongo.Collection('messages');
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Template.messages.helpers({
+    messages: function () {
+      return Messages.find({});
     }
   });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
